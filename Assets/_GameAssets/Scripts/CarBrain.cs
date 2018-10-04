e66da3b26d91a09ef7c8fae5e9553bc9ebcf6ca5
daher7 <daher7@gmail.com>
2018-10-04 13:43:57 +0200

    Initial commit

---
+++ Assets/_GameAssets/Scripts/CarBrain.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBrain : MonoBehaviour {
+
+    [SerializeField] int speed = 20;
+    [SerializeField] int speed_rotation = 5;
+    int marcha = 1;
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * marcha);
+        // Cuando vamos para atrás es cuando el coche gira.
+        if (marcha == -1)
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * speed_rotation);
+        }
+    }  
+
+    // Detectamos la colisión del coche contra los muros
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("He colisionado con: " + collision.gameObject.tag);
+
+        if(collision.gameObject.tag == "Pared")
+        {
+            // Colisiona con la pared
+            marcha *= -1;
+        }
+        else if (collision.gameObject.tag == "Target")
+        {
+            // He encontrado el objetivo
+            marcha = 0;
+            Debug.Log("DAVID.HT HA ENCONTRADO EL OBJETIVO");
+        }
+    }
+}
